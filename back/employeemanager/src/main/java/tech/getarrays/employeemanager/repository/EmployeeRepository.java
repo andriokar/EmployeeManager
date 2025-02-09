64935e6c596eee4342bf145b2106809b11c31508
Andriy <karpus.andrii@proton.me>
2025-02-09 20:20:52 +0100

    Create service

---
+++ back/employeemanager/src/main/java/tech/getarrays/employeemanager/repository/EmployeeRepository.java
@@ -0,0 +1,15 @@
+package tech.getarrays.employeemanager.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import tech.getarrays.employeemanager.model.Employee;
+
+import java.util.Optional;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+
+    Employee deleteEmployeeById(Long employeeId);
+
+    Optional<Employee> findEmployeeById(Long employeeId);
+}
---
+++ back/employeemanager/src/main/java/tech/getarrays/employeemanager/service/EmployeeService.java
@@ -0,0 +1,42 @@
+package tech.getarrays.employeemanager.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import tech.getarrays.employeemanager.model.Employee;
+import tech.getarrays.employeemanager.repository.EmployeeRepository;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
+public class EmployeeService {
+
+    private final EmployeeRepository employeeRepository;
+
+    @Autowired
+    public EmployeeService(EmployeeRepository employeeRepository) {
+        this.employeeRepository = employeeRepository;
+    }
+
+    public Employee addEmployee(Employee employee) {
+        employee.setEmployeeCode(UUID.randomUUID().toString());
+        return employeeRepository.save(employee);
+    }
+
+    public Employee updateEmployee(Employee employee) {
+        return employeeRepository.save(employee);
+    }
+
+    public Employee deleteEmployee(Long employeeId) {
+        return employeeRepository.deleteEmployeeById(employeeId);
+    }
+
+    public Employee findEmployeeById(Long employeeId) {
+        return employeeRepository.findEmployeeById(employeeId)
+                .orElseThrow(() -> new UserNotFoundException("User by id " + employeeId + " was not found"));
+    }
+
+    public List<Employee> findAllEmployees() {
+        return employeeRepository.findAll();
+    }
+}
