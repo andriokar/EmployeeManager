0e799689c4b6aa2fd7e0f86d3001174c78c7b92e
Andriy <karpus.andrii@proton.me>
2025-02-10 21:27:07 +0100

    Add Angular project with interface and service

--- back/employeemanager/src/main/java/tech/getarrays/employeemanager/repository/EmployeeRepository.java
+++ back/employeemanager/src/main/java/tech/getarrays/employeemanager/repository/EmployeeRepository.java
@@ -2,6 +2,7 @@ package tech.getarrays.employeemanager.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 import tech.getarrays.employeemanager.model.Employee;
 
 import java.util.Optional;
@@ -9,6 +10,7 @@ import java.util.Optional;
 @Repository
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
 
+    @Transactional
     Employee deleteEmployeeById(Long employeeId);
 
     Optional<Employee> findEmployeeById(Long employeeId);
